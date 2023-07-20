99213a2227b3837581fba50fb6e4725b7152851b
Eva Rusinova <119045926+EvaRusinova@users.noreply.github.com>
2023-07-20 19:47:31 +0300

    Stage 2 and 3 refactoring, added HTML and properties for mail

--- src/main/java/com/example/initial/event/UserRegistrationEvent.java
+++ src/main/java/com/example/initial/event/UserRegistrationEvent.java
@@ -2,7 +2,6 @@ package com.example.initial.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
 import java.io.Serializable;
 
 @Getter
--- src/main/java/com/example/initial/repository/UserRepository.java
+++ src/main/java/com/example/initial/repository/UserRepository.java
@@ -4,11 +4,8 @@ import com.example.initial.entity.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     User findByUserName(String userName);
-
     User findByEmail(String email);
 }
