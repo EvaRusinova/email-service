c0aebe94f72adf0edc3b63710b098b74e1ec71c2
Eva Rusinova <119045926+EvaRusinova@users.noreply.github.com>
2023-07-24 16:28:12 +0300

    email sending implemented successfully

--- src/main/java/com/example/initial/config/MailConfiguration.java
+++ src/main/java/com/example/initial/config/MailConfiguration.java
@@ -1,93 +1,63 @@
-//package com.example.initial.config;
-//
-//import com.google.api.client.http.javanet.NetHttpTransport;
-//import com.google.auth.http.HttpTransportFactory;
-//import com.google.auth.oauth2.UserCredentials;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.mail.javamail.JavaMailSenderImpl;
-//import org.springframework.stereotype.Component;
-//import org.thymeleaf.spring5.SpringTemplateEngine;
-//
-//import java.io.IOException;
-//import java.util.Properties;
-//
-//@Component
-//@Configuration
-//public class MailConfiguration {
-//
-//    @Value("${mail.host}")
-//    private String host;
-//
-//    @Value("${mail.port}")
-//    private int port;
-//
-//    @Value("${mail.username}")
-//    private String username;
-//
-//    @Value("${mail.password}")
-//    private String password;
-//
-//    @Value("${mail.properties.protocol}")
-//    private String protocol;
-//
-//    @Value("${mail.properties.auth}")
-//    private String smtpAuth;
-//
-//    @Value("${mail.properties.starttls}")
-//    private String starttls;
-//
-//    @Value("${mail.properties.debug}")
-//    private String debug;
-//
-//    @Value("${mail.oauth.clientId}")
-//    private String clientId;
-//
-//    @Value("${mail.oauth.clientSecret}")
-//    private String clientSecret;
-//
-//    @Value("${mail.oauth.refreshToken}")
-//    private String refreshToken;
-//
-//    @Bean
-//    public SpringTemplateEngine springTemplateEngine() {
-//        return new SpringTemplateEngine();
-//    }
-//
-//    @Bean
-//    public JavaMailSenderImpl getJavaMailSender() {
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//        mailSender.setHost(host);
-//        mailSender.setPort(port);
-//
-//        mailSender.setUsername(username);
-//        mailSender.setPassword(password);
-//
-//        Properties props = mailSender.getJavaMailProperties();
-//        props.put("mail.transport.protocol", protocol);
-//        props.put("mail.smtp.auth", smtpAuth);
-//        props.put("mail.smtp.starttls.enable", starttls);
-//        props.put("mail.debug", debug);
-//        mailSender.setJavaMailProperties(props);
-//
-//        try {
-//            HttpTransportFactory httpTransportFactory = NetHttpTransport::new;
-//            UserCredentials credentials = UserCredentials.newBuilder()
-//                    .setClientId(clientId)
-//                    .setClientSecret(clientSecret)
-//                    .setRefreshToken(refreshToken)
-//                    .setHttpTransportFactory(httpTransportFactory)
-//                    .build();
-//
-//            mailSender.setUsername(username);
-//            mailSender.setPassword(credentials.refreshAccessToken().getTokenValue());
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return mailSender;
-//    }
-//
-//}
+package com.example.initial.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import java.util.Properties;
+
+@Component
+@Configuration
+public class MailConfiguration {
+
+    @Value("${mail.host}")
+    private String host;
+
+    @Value("${mail.port}")
+    private int port;
+
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
+    @Value("${mail.properties.protocol}")
+    private String protocol;
+
+    @Value("${mail.properties.auth}")
+    private String smtpAuth;
+
+    @Value("${mail.properties.starttls}")
+    private String starttls;
+
+    @Value("${mail.properties.debug}")
+    private String debug;
+
+    @Bean
+    public SpringTemplateEngine springTemplateEngine() {
+        return new SpringTemplateEngine();
+    }
+
+    @Bean
+    public JavaMailSenderImpl getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", protocol);
+        props.put("mail.smtp.auth", smtpAuth);
+        props.put("mail.smtp.starttls.enable", starttls);
+        props.put("mail.debug", debug);
+        mailSender.setJavaMailProperties(props);
+
+        return mailSender;
+    }
+}
--- src/main/java/com/example/initial/listener/UserRegistrationEventListener.java
+++ src/main/java/com/example/initial/listener/UserRegistrationEventListener.java
@@ -4,6 +4,8 @@ import com.example.initial.entity.User;
 import com.example.initial.event.UserRegistrationEvent;
 import com.example.initial.messaging.EventPublisher;
 import com.example.initial.repository.UserRepository;
+import com.example.initial.service.MailSender;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -14,16 +16,20 @@ public class UserRegistrationEventListener {
 
     private final UserRepository userRepository;
     private final EventPublisher eventPublisher;
-//    private final MailSender mailSender;
+    private final MailSender mailSender;
 
     @RabbitListener(queues = "user-registration-queue")
-    public void onUserRegistrationEvent(UserRegistrationEvent event) {
+    public void onUserRegistrationEvent(UserRegistrationEvent event) throws MessagingException {
         User user = User.builder()
                 .userName(event.getUserName())
                 .email(event.getEmail())
                 .build();
         userRepository.save(user);
-//        mailSender.sendEmail(user);
+        try {
+            mailSender.sendEmail(user);
+        } catch (MessagingException e) {
+            // handle the exception
+        }
         eventPublisher.publishEvent("email-verification-exchange", "email-verification-key", event);
     }
 }
